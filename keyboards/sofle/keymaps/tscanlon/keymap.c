8a2d3e6265fce93a0dbd2152b0a3218da3e3e6f7
tawm <tscanlon.iv@gmail.com>
2023-02-27 20:27:10 -0800

    derp the screen

--- keyboards/sofle/keymaps/tscanlon/keymap.c
+++ keyboards/sofle/keymaps/tscanlon/keymap.c
@@ -21,7 +21,7 @@
 #include "encoder.c"
 
 // Base layer is the number of layers CYCLE selects from.
-#define BASE_LAYERS 2
+#define BASE_LAYERS 3
 
 enum custom_keycodes {
     PLACEHOLDER = SAFE_RANGE,  // can always be here (4 bytes)
@@ -30,6 +30,7 @@ enum custom_keycodes {
 
 enum custom_layers {
     _QWERTY,
+    _WORK,
     _COLEMAK,
     _LOWER,
     _RAISE
@@ -58,66 +59,38 @@ enum custom_layers {
 
 //Default keymap. This can be changed in Via. Use oled.c to change beavior that Via cannot change.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
 [_QWERTY] = LAYOUT_via(
    KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                         KC_6    , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_VOLU,       KC_PGUP, KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
    KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_MUTE,       KC_NO  , KC_H    , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,
    KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
-                  KC_LGUI,KC_LALT , MO(_LOWER), KC_SPC, KC_ENT,           KC_TAB  , KC_BSPC, MO(_RAISE),KC_RCTL, KC_RGUI
+                  KC_LGUI,KC_LALT , MO(_LOWER), KC_SPC, KC_ENT,           KC_TAB  , KC_BSPC, MO(_LOWER),KC_RCTL, KC_RGUI
+),
+
+[_WORK] = LAYOUT_via(
+   KC_ESC , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                         KC_6    , KC_7   , KC_8   , KC_9   , KC_0   , KC_GRV ,
+   KC_DEL , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_VOLU,       KC_PGUP, KC_Y    , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+   KC_NO, KC_A   , LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), LGUI_T(KC_G), KC_MUTE,       KC_NO  , RGUI_T(KC_H)    , RSFT_T(KC_J)   , RCTL_T(KC_K)  , RALT_T(KC_L)   , KC_SCLN, KC_QUOT,
+   KC_NO, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN, KC_N    , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT,
+                  KC_LGUI,KC_LALT , MO(_LOWER), KC_SPC, KC_ENT,           KC_TAB  , KC_BSPC, MO(_LOWER),KC_RCTL, KC_RGUI
 ),
 
 [_COLEMAK] = LAYOUT_via(
    KC_ESC, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                          KC_6    , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO,
    KC_DEL, KC_Q   , KC_W   , KC_F   , KC_P   , KC_G   , KC_VOLU,       KC_PGUP,  KC_J    , KC_L   , KC_U   , KC_Y   , KC_SCLN, KC_NO,
-   KC_NO , KC_A   , KC_R   , KC_S   , KC_T   , KC_D   , KC_MUTE,       KC_NO  ,  KC_H    , KC_N   , KC_E   , KC_I   , KC_O   , KC_NO,
+   KC_NO, KC_A   , LALT_T(KC_R), LCTL_T(KC_S), LSFT_T(KC_T), LGUI_T(KC_D), KC_MUTE,       KC_NO  , RGUI_T(KC_H)    , RSFT_T(KC_N)   , RCTL_T(KC_E)  , RALT_T(KC_I)   , KC_O, KC_NO,
    KC_NO , KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_VOLD,       KC_PGDN,  KC_N    , KC_M   , KC_COMM,  KC_DOT, KC_SLSH, KC_NO,
-                  KC_LGUI,KC_LALT , MO(_LOWER), KC_SPC, KC_ENT,           KC_TAB  , KC_BSPC, MO(_RAISE),KC_RCTL, KC_RGUI
+                  KC_LGUI,KC_LALT , MO(_LOWER), KC_SPC, KC_ENT,           KC_TAB  , KC_BSPC, MO(_LOWER),KC_RCTL, KC_RGUI
 ),
 
-
-/* LOWER
- * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |  F1  |  F2  |  F3  |  F4  |  F5  |-------.  E  ,-------|  F6  |  F7  |  F8  |  F9  | F10  | F11  |
- * |------+------+------+------+------+------|       |< N >|       |------+------+------+------+------+------|
- * |  `   |   1  |   2  |   3  |   4  |   5  |-------.  C  ,-------|   6  |   7  |   8  |   9  |   0  | F12  |
- * |------+------+------+------+------+------|       |< O >|       |------+------+------+------+------+------|
- * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.  D  ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
- * |------+------+------+------+------+------|       |< E >|       |------+------+------+------+------+------|
- * | Shift|  =   |  -   |  +   |   {  |   }  |-------|  R  |-------|   [  |   ]  |   ;  |   :  |   \  |      |
- * `-----------------------------------------/       /      \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter  /        \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/       /          \      \ |      |      |      |      |
- *            `-----------------------------------'            '------''---------------------------'
- */
 [_LOWER] = LAYOUT_via(
-  _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
-  _______, KC_NO, KC_NO  , KC_BSLS, KC_LBRC , KC_GRV, _______,       _______, KC_NO, KC_RBRC, KC_QUOT, KC_NO, KC_NO, KC_F12,
-  KC_NO  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , _______,       _______, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO ,
-  _______, KC_NO , KC_NO, KC_NO, KC_MINUS, KC_EQL, _______,       _______, KC_LEFT, KC_UP, KC_DOWN, KC_LEFT, KC_NO, _______,
+  CYCLE, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,
+  KC_CAPS, KC_NO, KC_NO  , KC_BSLS, KC_LBRC , KC_GRV,   RGB_MOD,    RGB_VAI, KC_NO, KC_RBRC, KC_QUOT, KC_NO, KC_NO, KC_F12,
+  KC_NO  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , _______,    RGB_TOG, KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_NO ,
+  _______, KC_NO , KC_NO, KC_NO, KC_MINUS, KC_EQL,      RGB_RMOD,   RGB_VAD, KC_LEFT, KC_UP, KC_DOWN, KC_LEFT, KC_NO, _______,
                   _______, _______, _______, _______, _______,           _______, _______, _______, _______, _______
 ),
-/* RAISE
- * ,----------------------------------------.                      ,-----------------------------------------.
- * |Cycle |      |      |      |      |      |-------.  E  ,-------|      |      |      |      |      |      |
- * |------+------+------+------+------+------|       |< N >|       |------+------+------+------+------+------|
- * | Esc  | Ins  | Pscr | Menu |      |      |-------.  C  ,-------|      |      |  Up  |      | DLine| Bspc |
- * |------+------+------+------+------+------|       |< O >|       |------+------+------+------+------+------|
- * | Tab  | LAt  | LCtl |LShift|      | Caps |-------.  D  ,-------|      | Left | Down | Rigth|  Del | Bspc |
- * |------+------+------+------+------+------|       |< E >|       |------+------+------+------+------+------|
- * |Shift | Undo |  Cut | Copy | Paste|      |-------|  R  |-------|      | LStr |      | LEnd |      | Shift|
- * `-----------------------------------------/      /       \      \-----------------------------------------'
- *            | LGUI | LAlt | LCTR |LOWER | /Enter /         \Space \  |RAISE | RCTR | RAlt | RGUI |
- *            |      |      |      |      |/      /           \      \ |      |      |      |      |
- *            `----------------------------------'             '------''---------------------------'
- */
-[_RAISE] = LAYOUT_via(
-    CYCLE, _______, _______, _______, _______ , _______,                       _______, _______, _______, _______, _______, _______,
-  _______, KC_INS , KC_PSCR, KC_APP , XXXXXXX , XXXXXXX, _______,    _______,  KC_PGUP, _______, KC_UP  , _______, _______, KC_BSPC,
-  _______, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX , KC_CAPS, _______,    _______,  KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL , KC_BSPC,
-  _______, KC_UNDO, KC_CUT , KC_COPY, KC_PASTE, XXXXXXX, _______,    _______,  XXXXXXX, _______, XXXXXXX, _______, XXXXXXX, _______,
-                   _______, _______, _______, _______, _______,        _______, _______, _______, _______, _______
-)
+
 };
 
 // Custom keycode handling.
